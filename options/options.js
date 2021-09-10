54a62e489fbdc5f5b8212641dd4c739f49fb5388
Crowdbotics <team@crowdbotics.com>
2021-09-10 12:13:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "rapid_queen_30434Identifier",
+  url: "https://rapid_queen_30434Identifier.botics.co",
+  api: "https://rapid_queen_30434Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
